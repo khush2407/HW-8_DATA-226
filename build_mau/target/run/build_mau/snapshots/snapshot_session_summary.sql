261298e0a72b642952d6a468897215fd5e3dd7d7
khush2407 <88429450+khush2407@users.noreply.github.com>
2024-10-30 18:35:31 -0700

    Add files via upload

---
+++ build_mau/target/run/build_mau/snapshots/snapshot_session_summary.sql
@@ -0,0 +1,32 @@
+
+      
+  
+    
+
+        create or replace transient table DEV.snapshot.snapshot_session_summary
+         as
+        (
+    
+
+    select *,
+        md5(coalesce(cast(sessionId as varchar ), '')
+         || '|' || coalesce(cast(ts as varchar ), '')
+        ) as dbt_scd_id,
+        ts as dbt_updated_at,
+        ts as dbt_valid_from,
+        nullif(ts, ts) as dbt_valid_to
+    from (
+        
+
+  
+
+SELECT * FROM DEV.ANALYTICS.session_summary
+
+    ) sbq
+
+
+
+        );
+      
+  
+  
